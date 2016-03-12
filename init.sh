d395ca0a9e68a2f75db5f0525ebd677b2ef39233
stepic154 <1235678@mail.ru>
2016-03-12 22:15:59 +0500

    3-4

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ sudo /etc/init.d/nginx restart
 
 # gunicorn
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/wsgi.conf /etc/gunicorn.d/wsgi.py
+sudo ln -sf /home/box/web/etc/gunicorn/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
 gunicorn hello:app -b 0.0.0.0:8080 -D
-gunicorn -c wsgi -b 0.0.0.0:8000 -D
+gunicorn -c ask
