5a32e3b1859a2d29096dd7e93d6d28b49f2cb3e5
stepic154 <1235678@mail.ru>
2016-03-12 14:59:28 +0500

    2-3

--- init.sh
+++ init.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+# gunicorn
+#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.conf
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
