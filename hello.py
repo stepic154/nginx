792842f409d101007f45258461f576b6494bcd40
stepic154 <1235678@mail.ru>
2016-03-12 14:46:01 +0500

    2-2

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def wsgi_application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
 	
-    body = s[2:].split(sep='&')
+    body = environ['QUERY_STRING'].split("&")
 	
     start_response(status, headers )
     return body
