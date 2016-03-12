1d09d955e452294a6c109fc3b0acef8f8d6621e1
stepic154 <1235678@mail.ru>
2016-03-12 22:07:45 +0500

    3-3

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ sudo /etc/init.d/nginx restart
 
 # gunicorn
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/ask/ask/wsgi.py /etc/gunicorn.d/dgango.py
+sudo ln -sf /home/box/web/etc/wsgi.conf /etc/gunicorn.d/wsgi.py
 sudo /etc/init.d/gunicorn restart
 gunicorn hello:app -b 0.0.0.0:8080 -D
-gunicorn django:application -b 0.0.0.0:8000 -D
+gunicorn -c wsgi -b 0.0.0.0:8000 -D
