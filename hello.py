7db9a39cabc74e97539583abf25455ca75cb66e1
stepic154 <1235678@mail.ru>
2016-03-12 16:18:06 +0500

    2-8

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
-def wsgi_application(environ, start_response):
+bind="0.0.0.0:8080"
+logfile = '/home/box/web/gunicorn.log'
+errorlog = '/home/box/web/gunicorn_error.log'
+
+def app (environ, start_response):
     status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
-	
-    body = [item + "\r\n" for item in environ['QUERY_STRING'].split("&")]
-	
-    start_response(status, headers)
-    return body
+    response_headers = [('Content-type','text/plain')]
+    
+    body = '\r\n'.join(environ['QUERY_STRING'].split('&'))
+    
+    start_response(status, response_headers)
+    return [options]
+
 
--- init.sh
+++ init.sh
@@ -5,6 +5,4 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 # gunicorn
-#sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.conf
-
-#gunicorn -b 0.0.0.0:8080 --python path hello
+sudo ln -sf /home/box/web/hello.conf /etc/gunicorn.d/hello.py
