f0ca4d6ed4362ca906345dbbec2e7e16288c7e85
stepic154 <1235678@mail.ru>
2016-03-12 16:03:55 +0500

    2-7

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ def wsgi_application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
 	
-    body = environ['QUERY_STRING'].split("&")
+    body = [item + "\r\n" for item in environ['QUERY_STRING'].split("&")]
 	
-    start_response(status, headers )
+    start_response(status, headers)
     return body
 
--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
- 
+
 # nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 # gunicorn
-sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.conf
+#sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.conf
+
+#gunicorn -b 0.0.0.0:8080 --python path hello
