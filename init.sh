5cac759061c9a6470c12d416f018229e799dc3b3
stepic154 <1235678@mail.ru>
2016-03-20 09:40:53 +0500

    16

--- init.sh
+++ init.sh
@@ -6,10 +6,10 @@
 sudo /etc/init.d/gunicorn restart
 
 cd /home/box/web
-sudo gunicorn --bind 0.0.0.0:8000 --access-logfile /tmp/acc_hello.log --error-logfile /tmp/err_hello.log hello:app
+sudo gunicorn --bind 0.0.0.0:8000 --access-logfile /tmp/acc_hello.log --error-logfile /tmp/err_hello.log -D hello:app
 
 cd /home/box/web/etc/ask
-sudo gunicorn --bind 0.0.0.0:8080 --access-logfile /tmp/acc_ask.log --error-logfile /tmp/err_ask.log ask.wsgi:application
+sudo gunicorn --bind 0.0.0.0:8080 --access-logfile /tmp/acc_ask.log --error-logfile /tmp/err_ask.log -D ask.wsgi:application
 
 # nginx
 sudo rm -rf /etc/nginx/sites-enabled/default
