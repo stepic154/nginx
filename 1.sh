6742bd2ef9b94277bc5fe1ee6958f44eeb4c0ecb
stepic154 <1235678@mail.ru>
2016-03-20 10:04:40 +0500

    20

--- 1.sh
+++
@@ -1,2 +0,0 @@
-cd /home/box/web
-sudo gunicorn --bind 0.0.0.0:8000 --access-logfile /tmp/acc_hello.log --error-logfile /tmp/err_hello.log -D hello:app
--- 2.sh
+++
@@ -1,2 +0,0 @@
-cd /home/box/web/ask
-sudo gunicorn --bind 0.0.0.0:8080 --access-logfile /tmp/acc_ask.log --error-logfile /tmp/err_ask.log -D ask.wsgi:application
