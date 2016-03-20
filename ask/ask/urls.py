c610cc5d6d0f7b0e1a80e38e6b2d069fae4e04f8
stepic154 <1235678@mail.ru>
2016-03-20 11:09:11 +0500

    22

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -4,16 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    
+    url(r'^/$', 'qa.views.test', name='root'),
     url(r'^login/.*$', 'qa.views.test', name='login'),
     url(r'^signup/.*$', 'qa.views.test', name='signup'),
     url(r'^question/\d+/$', 'qa.views.test', name='question'),
     url(r'^ask/.*$', 'qa.views.test', name='ask'),
     url(r'^popular/.*$', 'qa.views.test', name='popular'),
     url(r'^new/.*$', 'qa.views.test', name='new'),
-    url(r'^/$', 'qa.views.test', name='root'),
     url(r'^admin/', include(admin.site.urls)),
 )
