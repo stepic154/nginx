d15968c4081ae6d8c0fc96518521cde014c12f9c
stepic154 <1235678@mail.ru>
2016-03-20 09:37:23 +0500

    15

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 
 # gunicorn
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
-
-cd /home/box/web
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
-
-cd /home/box/web/etc/ask
-sudo gunicorn -c /etc/gunicorn.d/ask.py ask.wsgi:application
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 
 sudo /etc/init.d/gunicorn restart
 
+cd /home/box/web
+sudo gunicorn --bind 0.0.0.0:8000 --access-logfile /tmp/acc_hello.log --error-logfile /tmp/err_hello.log hello:app
+
+cd /home/box/web/etc/ask
+sudo gunicorn --bind 0.0.0.0:8080 --access-logfile /tmp/acc_ask.log --error-logfile /tmp/err_ask.log ask.wsgi:application
+
 # nginx
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+etcsudo /etc/init.d/nginx restart
