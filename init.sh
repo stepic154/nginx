d4476b13e8e0c6ac179244954424f08d571e479f
stepic154 <1235678@mail.ru>
2016-03-20 09:43:18 +0500

    17

--- init.sh
+++ init.sh
@@ -8,10 +8,10 @@ sudo /etc/init.d/gunicorn restart
 cd /home/box/web
 sudo gunicorn --bind 0.0.0.0:8000 --access-logfile /tmp/acc_hello.log --error-logfile /tmp/err_hello.log -D hello:app
 
-cd /home/box/web/etc/ask
+cd /home/box/web/ask
 sudo gunicorn --bind 0.0.0.0:8080 --access-logfile /tmp/acc_ask.log --error-logfile /tmp/err_ask.log -D ask.wsgi:application
 
 # nginx
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-etcsudo /etc/init.d/nginx restart
+sudo /etc/init.d/nginx restart
